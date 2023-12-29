f96e0ddcfeaaaba02c85bb18b63d29714d500c4e
Rafael Estrella <123432865+RafaelEstrella05@users.noreply.github.com>
2023-12-29 14:08:21 -0600

    model view controller initial dummy set up

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from View.View import View
 from Controller.Controller import Controller
 
 def main():
-    model = Model()
-    controller = Controller(model, None)
-    view = View(controller)
+    model = Model() # create Model
+    controller = Controller(model, None), # create Controller, pass reference to Model
+    view = View(controller) # create View
     controller.view = view  # set view in controller
 
-    view.mainloop()
+    view.mainloop()  # start application
 
 if __name__ == "__main__":
     main()
